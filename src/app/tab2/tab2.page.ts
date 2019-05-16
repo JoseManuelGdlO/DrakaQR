5abf37d33d77c07ce6a240af530d361b3b7f379b
UriAlcantar <1404126@itdurango.edu.mx>
2019-05-16 12:34:00 -0500

    Cambios a tabs

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController, AlertController } from '@ionic/angular';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-tab2',
@@ -7,11 +9,186 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab2Page implements OnInit{
 
-  constructor() { }
+  public noSeries: any;
+  idCard: number;
+  scannedData: string;
+  codigoRack: string;
+  letrauno: boolean;
+  letrados: boolean;
+  letratres: boolean;
+  letracuatro: boolean;
+  estado: string;
+  jsonDataLector: any;
+  codigo: string;
+  producto: string;
+  proveedor: string;
+  anioProduccion: string;
+  noSerieProducto: string;
+
+  constructor(
+    private barcodeScanner: BarcodeScanner,
+    private toastCtrl: ToastController,
+    private alertCtrl: AlertController
+) { 
+  this.noSeries = [];
+  this.jsonDataLector = [];
+  this.idCard = 0;
+
+  this.letrauno = false;
+  this.letrados = false;
+  this.letratres = false;
+  this.letracuatro = false;
+  
+
+  this.estado = "D";
+}
 
   ngOnInit() {
   }
+  async presentToast(mensaje : string, pos: any, color: string) {
+    const toast = await this.toastCtrl.create({
+      message : mensaje,
+      position : pos,
+      duration : 2000,
+      color: color
+    });
+    toast.present();
+    
+  }
 
+  escanearRack(codigo: string){
+    
+    
+
+
+    this.producto = codigo.charAt(0);
+    this.proveedor = codigo.charAt(1) + codigo.charAt(2) + codigo.charAt(3)
+    +codigo.charAt(4) + codigo.charAt(5) + codigo.charAt(6) + codigo.charAt(7);
+    this.anioProduccion = codigo.charAt(8) + codigo.charAt(9);
+    this.noSerieProducto = codigo.charAt(10) + codigo.charAt(11) + codigo.charAt(12) +
+    codigo.charAt(13) + codigo.charAt(14) + codigo.charAt(15) + codigo.charAt(16) + 
+    codigo.charAt(17) + codigo.charAt(18)
+
+    this.idCard = this.idCard + 1;
+    this.noSeries.push({
+      id_card: this.idCard , 
+      serie: codigo, 
+      proveedro: this.proveedor,
+      anio: this.anioProduccion,
+      seriem: this.noSerieProducto
+    })
+
+  }
+
+  scanCodeRack(){
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        
+        this.codigoRack = barcodeData.text;
+        
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }
+
+  escanearRacktest(){
+    this.idCard = this.idCard + 1;
+    this.noSeries.push({
+      id_card: this.idCard , 
+      serie: "12131412", 
+      estado: "Pasado",
+      proveedro: "NAIKI",
+      anio: "2069",
+      seriem: "SNAIKI6900123423"
+    })
+  }
+
+  borrar(idSerie: number){
+    this.noSeries.splice(idSerie,1);
+    console.log(this.noSeries);
+  }
+
+  scanCode() {
+    this.barcodeScanner
+      .scan()
+      .then(barcodeData => {
+        
+        this.scannedData = barcodeData.text;
+        
+        
+
+        this.escanearRack(this.scannedData);
+
+        
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+  }
+
+ 
+  cancelarChavos(){
+    this.noSeries= [];
+    this.codigoRack = "";
+
+  }
+  guardarChido(){
+    if (!this.codigoRack) {
+      this.presentToast("Ingrese un codigo de Rack","bottom", "danger");
+    }else if (this.codigoRack.length < 4) {
+      this.presentToast("El codigo debe ser de 4 digitos", "bottom", "danger");
+      
+    }else if (this.codigoRack.length > 4) {
+      this.presentToast("Longitud de codigo de Rack es mayor a 4", "bottom", "danger");
+    } else if (this.codigoRack.length==4) {
+      var str = new String(this.codigoRack);
+      var res = str.charAt(0)
+      var number = parseInt(res);
+      var resd = new String(number);
+      
+      if 
+        (resd == "NaN") {
+        this.letrauno = true;
+        
+        var res = str.charAt(1);
+        var number = parseInt(res);
+        var resd = new String(number);
+        
+        if (resd != "NaN") {
+        var res = str.charAt(2);
+        var number = parseInt(res);
+        var resd = new String(number);
+
+        if (resd != "NaN") {
+        var res = str.charAt(3);
+        var number = parseInt(res);
+        var resd = new String(number);
+
+        if (resd == "NaN") {
+          console.log(resd);
+          console.log("Cuarta bien");
+        this.presentToast("Codigo Correcto","top","primary");
+        //to go code
+        }else{
+          this.presentToast("Verifique cuarta letra de código","bottom","danger");
+        }
+
+        }else{
+          this.presentToast("Verifique tercera letra de código","bottom","danger");
+        }
+        }else{
+          this.presentToast("Verifique segunda letra de código","bottom","danger");
+        }
+      }else{
+        this.presentToast("Verifique primera letra de código", "bottom", "danger")
+      }
+      
+      
+    }
+
+  }
   
 
 }
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage{
+
+  id: any;
+
+  constructor(
+    public router: Router,
+    public route: ActivatedRoute,
+  ){
+    this.route.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+
+
+        this.id = this.router.getCurrentNavigation().extras.state.id_usuario;
+        console.log("ID USUARIO  "+this.id);
+      }
+    });
+
+  }
+
+  mnss(){
+    console.log("ados mund crue");
+    this.router.navigateByUrl('mensajes');
+  }
+
+  
+}
