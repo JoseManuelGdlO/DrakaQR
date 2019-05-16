9acafb70f7d18677376ffdbd469dc347ffc4cf18
UriAlcantar <1404126@itdurango.edu.mx>
2019-05-16 12:54:51 -0500

    Cambios

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -109,6 +109,17 @@ mostrarUtlimoCambio(){
        });
   });
 }
+insertarProducto(serie: string, noRack: string, idUsuario: string, status: string){
+  var url = this.inicioURL+'insertarProducto/'+ serie+ '/' + noRack + '/' + idUsuario + '/' + status;
+  return new Promise((resolve, reject) => {
+   this.http.get(url)
+      .subscribe(data => {
+        resolve(data);
+       }, (err) =>{
+         reject(err);    
+       });
+  });
+}
 
 
 }
